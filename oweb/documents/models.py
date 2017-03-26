15d2f50ad2950fd7039b63f6a33368ab08dfa686
Ricardo M. Lima <ricardolima@neemu.com>
2017-03-26 01:41:15 -0300

    complete model and migration

--- oweb/documents/models.py
+++ oweb/documents/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class Document(models.Model):
-    url = models.CharField(max_length=200)
-    document_owner = models.ForeignKey(UserProfile, verbose_name="User")
-
-    def __str__ (self):
-        return self.url
-
 class UserProfile(models.Model):
     name = models.CharField(max_length=50, verbose_name="Name")
     email = models.EmailField(verbose_name="Email")
@@ -16,3 +8,11 @@ class UserProfile(models.Model):
     born_date = models.DateField(verbose_name="Born date", null=True, default=None, blank=True)
     last_connection = models.DateTimeField(verbose_name="Date of last connection", null=True, default=None, blank=True)
     date_created = models.DateField(verbose_name="Date of Birthday", auto_now_add=True)
+
+# Create your models here.
+class Document(models.Model):
+    url = models.CharField(max_length=200)
+    document_owner = models.ForeignKey(UserProfile, verbose_name="User", null=True, blank=False, default=None)
+
+    def __str__ (self):
+        return self.url
