ecf341bd0dcadf254f6b32bc95f4f830f66394ce
Ricardo M. Lima <ricardolima@neemu.com>
2017-04-25 01:43:08 -0300

    removing user profile and adding category in documents app

--- oweb/documents/models.py
+++ oweb/documents/models.py
@@ -1,22 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user_auth = models.OneToOneField(User, primary_key=True)
-    born_date = models.DateField(verbose_name="Born date", null=True, default=None, blank=True)
-    last_connection = models.DateTimeField(verbose_name="Date of last connection", null=True, default=None, blank=True)
-    date_created = models.DateField(verbose_name="Date of Birthday", auto_now_add=True)
 
-    def __str__(self):
-        return self.user_auth.username
-
-# Create your models here.
-class Document(models.Model):
-    url = models.CharField(max_length=200)
+class Category(models.Model):
+    name = models.CharField(max_length=200)
     owner = models.ForeignKey(User, null=False, default=None)
 
     def __str__ (self):
+        return self.name
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+
+class Document(models.Model):
+    url = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, null=False, default=None)
+    category = models.ForeignKey(Category, null=False, default=None)
+
+    def __str__ (self):
         return self.url
+
     class Meta:
         verbose_name = "document"
         verbose_name_plural = "documents"
